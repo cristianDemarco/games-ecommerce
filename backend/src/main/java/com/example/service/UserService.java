bd8c56d6587bd0dda48d0c823009bab93e7b20b3
cristianDemarco <demarcocristian07@gmail.com>
2026-02-17 16:22:14 +0100

    feat: add frontend configuration refactor: modify project structure

--- backend/src/main/java/com/example/service/UserService.java
+++ backend/src/main/java/com/example/service/UserService.java
@@ -2,7 +2,6 @@ package com.example.service;
 
 import com.example.model.Cart;
 import com.example.model.User;
-import com.example.repository.CartRepository;
 import com.example.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,7 +11,6 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
     @Autowired
-    private CartRepository cartRepository;
 
     public User saveUserWithCart(User user){
         Cart cart = new Cart();
