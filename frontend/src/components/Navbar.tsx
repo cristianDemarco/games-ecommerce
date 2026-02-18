87f255f656e592b32b2b95ca85c574a372142588
cristianDemarco <demarcocristian07@gmail.com>
2026-02-18 22:40:13 +0100

    feat: navbar and logo

---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,40 @@
+const Navbar = () => {
+    return (
+        <nav className="navbar bg-body-tertiary fixed-top" data-bs-theme="dark">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">
+                    <img src="src/assets/logo-isygame.png" alt="Logo" width="100" height="100" className="d-inline-block align-text-middle"></img>
+                    <h3 className="d-inline-block">Isygame</h3>
+                </a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="offcanvas offcanvas-end" tabIndex={-1} id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+                <div className="offcanvas-header">
+                    <h5 className="offcanvas-title" id="offcanvasNavbarLabel">Isygame</h5>
+                    <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                </div>
+                <div className="offcanvas-body">
+                    <ul className="navbar-nav justify-content-end flex-grow-1 px-3">
+                    <li className="nav-item">
+                        <a className="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Cart</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Profile</a>
+                    </li>
+                    </ul>
+                    <form className="d-flex mt-3" role="search">
+                    <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+                    <button className="btn btn-outline-success" type="submit">Search</button>
+                    </form>
+                </div>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
